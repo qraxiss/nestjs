a93fbc92b10c05d2c06d223a85e305b8ebf3e093
fatih <qraxiss@gmail.com>
2025-05-15 17:59:37 +0300

    users downgraded to stone age & hardcoded

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,28 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../user/user.service';
-import * as bcrypt from 'bcrypt';
 import { EnvService } from 'src/env/env.service';
 
 @Injectable()
 export class AuthService {
     constructor(
-        private usersService: UserService,
         private jwtService: JwtService,
         private envService: EnvService,
     ) { }
 
     async validateUser(email: string, pass: string): Promise<any> {
-        const user = await this.usersService.findOneByEmail(email);
-        if (user && (await bcrypt.compare(pass, user.password))) {
-            const { password, ...result } = user;
-            return result;
+        const user = UserService.USERS.find(user => user.email === email);
+        if (user && user.password === pass) {
+            return user;
         }
         return null;
     }
 
     async login(user: any) {
-        const payload = { email: user.email, sub: user.id, isAdmin: user.isAdmin };
+        const payload = { email: user.email, sub: user.email };
         return {
             access_token: this.jwtService.sign(payload, {
                 secret: this.envService.envConfig.JWT_KEY
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
-import { UserController } from 'src/user/user.controller';
 import { UserService } from 'src/user/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/user/user.entity';
-import { SettingModule } from './setting/setting.module';
 
 @Module({
-  controllers: [UserController],
   providers: [UserService],
   imports: [
-    TypeOrmModule.forFeature([User]),
-    SettingModule,
   ],
-  exports: [TypeOrmModule]
 })
 export class UserModule { }
